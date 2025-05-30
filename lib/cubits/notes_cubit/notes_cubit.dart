bcf725a53b4d1bbcb525e8b690582c12e97c176a
Abdelrahman <abdosaid3495@gmail.com>
2025-05-30 17:57:28 +0300

    display notes part one

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -7,15 +7,11 @@ import 'package:notes_app/models/note_model.dart';
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
+  List<NoteModel>? notes;
   NotesCubit() : super(NotesInitial());
-  fetchAllNotes(){
-      emit(NotesLoading());
-    try {
-      var notesBox = Hive.box<NoteModel>(kNotesBox);
-      
-      emit(NotesSeccess(notesBox.values.toList()));
-    } catch (e) {
-      emit(NotesFailure(e.toString()));
-    }
+  fetchAllNotes() {
+    var notesBox = Hive.box<NoteModel>(kNotesBox);
+
+    notes = notesBox.values.toList();
   }
 }
--- lib/views/widget/notes_view_body.dart
+++ lib/views/widget/notes_view_body.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/views/widget/custom_app_bar.dart';
 import 'package:notes_app/views/widget/custom_note_list_view.dart';
 
-class NotesViewBody extends StatelessWidget {
+class NotesViewBody extends StatefulWidget {
   const NotesViewBody({super.key});
+
+  @override
+  State<NotesViewBody> createState() => _NotesViewBodyState();
+}
+
+class _NotesViewBodyState extends State<NotesViewBody> {
+  @override
+  void initState() {
+    super.initState();
+    BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: const Column(
-        children: [SizedBox(height: 50), CustomAppBar(tittle: 'Notes', icon: Icons.search,), Expanded(child: NoteListView())],
+        children: [
+          SizedBox(height: 50),
+          CustomAppBar(tittle: 'Notes', icon: Icons.search),
+          Expanded(child: NoteListView()),
+        ],
       ),
     );
   }
 }
-
